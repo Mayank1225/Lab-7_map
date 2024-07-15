9ae914f1ac28f1f306bf76a63325682ca59edd4b
Mayank1225 <mayankmalviya2169@gmail.com>
2024-07-15 15:07:48 -0700

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,98 @@
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var currentSpeed: UILabel!
+    @IBOutlet weak var maxSpeed: UILabel!
+    @IBOutlet weak var avgSpeed: UILabel!
+    @IBOutlet weak var Distance: UILabel!
+    @IBOutlet weak var maxAccelerate: UILabel!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var redview: UIView!
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var startTripLocation: CLLocation?
+    var preSpeed: CLLocationSpeed?
+    var accelerationValue: Double = 0
+    var speedValue: CLLocationSpeed = 0
+    var startTime: Date?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        currentSpeed.text = "0.00 km/h"
+        maxSpeed.text = "0.00 km/h"
+        avgSpeed.text = "0.00 km/h"
+        Distance.text = "0.00 km"
+        maxAccelerate.text = "0.00 m/s^2"
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        map.showsUserLocation = true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: 500, longitudinalMeters: 500)
+        if startTripLocation == nil {
+            speedValue = 0
+            startTripLocation = location
+        }
+        
+        let speed = location.speed
+        if speed > speedValue {
+            speedValue = speed
+        }
+        
+        redview.backgroundColor = speed * 3.6 > 115 ? .red : .systemGray6
+        
+        currentSpeed.text = "\(String(format: "%.2f", speed * 3.6)) km/h"
+        maxSpeed.text = "\(String(format: "%.2f", speedValue * 3.6)) km/h"
+        
+        let tripDistance = location.distance(from: startTripLocation!) / 1000
+        Distance.text = "\(String(format: "%.2f", tripDistance)) km"
+        
+        if let startTime = startTime {
+            let timeDistance = Date().timeIntervalSince(startTime)
+            let averageSpeed = tripDistance / (timeDistance / 3600)
+            avgSpeed.text = "\(String(format: "%.2f", averageSpeed)) km/h"
+        }
+        
+        calculateAcceleration(CurrentSpeed: speed)
+        
+        map.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @IBAction func onClickStart(_ sender: Any) {
+        startTime = Date()
+        greenView.backgroundColor = .green
+        locationManager.startUpdatingLocation()
+    }
+    
+    @IBAction func onClickStop(_ sender: Any) {
+        greenView.backgroundColor = .gray
+        redview.backgroundColor = .systemGray6
+        startTripLocation = nil
+        startTime = nil
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func calculateAcceleration(CurrentSpeed: CLLocationSpeed) {
+        if preSpeed == nil {
+            preSpeed = CurrentSpeed
+        }
+        let acceleration = abs(CurrentSpeed - preSpeed!)
+        preSpeed = CurrentSpeed
+        
+        if acceleration > accelerationValue {
+            accelerationValue = acceleration
+            maxAccelerate.text = "\(String(format: "%.2f", accelerationValue * 3.6)) m/s^2"
+        }
+    }
+}
